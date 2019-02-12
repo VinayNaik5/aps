efad6f248ffd5fb7a01ee10810e7b5fea0b19ac4
Vinay NAik <35489499+VinayNaik5@users.noreply.github.com>
2019-02-12 16:05:33 +0530

    Update generating_subsets_bitManipilation.c

--- generating_subsets_bitManipilation.c
+++ generating_subsets_bitManipilation.c
@@ -1,35 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
+
 int main()
 {
-   int n;
-   scanf("%d",&n);
-   int arr[n];
-   int temp[n];
-   //memset(temp,1,sizeof(temp));
-   for(int i=0;i<n;i++){
-    temp[i]=1;
-   }
-   for(int i=0;i<n;i++){
-    scanf("%d",&arr[i]);
-   }
-   for(int i=1;i<n;i++){
-    for(int j=0;j<i;j++){
-        if(arr[j]<arr[i]){
-            if(temp[j]+1>temp[i]){
-                temp[i]=temp[j]+1;
-            }
+    char data[]={'a','b','c'};
+    int set_size=3;
+    unsigned int size=pow(2,set_size);
+    int i,j;
+    for(i=0;i<size;i++){
+        for(j=0;j<set_size;j++){
+            if(i&(1<<j))
+                printf("%c ",data[j]);
         }
+        printf("\n");
     }
-   }
-   int max=1;
-   for(int i=1;i<n;i++){
-    if(temp[i]>max){
-        max=temp[i];
 
-    }
-   printf("\n");
-   }
-   printf("max sub sequence : %d",max);
 }
